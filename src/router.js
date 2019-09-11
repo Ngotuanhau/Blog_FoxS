a77f42be0349d20cd0848d0d25a886318dcb9c00
tuanhau <ngotuanhaudn@gmail.com>
2019-09-11 16:00:58 +0700

    Tạo store và router

--- src/router.js
+++ src/router.js
@@ -1,25 +1,25 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+/* eslint-disable */
 
-Vue.use(Router)
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
 
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
-})
+Vue.use(Router);
+
+let router = new Router({
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes: [{
+        path: "/",
+        component: {
+            render: h => h("router-view")
+        },
+        children: [{
+            path: "/",
+            name: "home",
+            component: Home
+        }]
+    }]
+});
+
+export default router;
---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import Vuex from "vuex";
+import Vue from "vue";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    modules: {}
+});
