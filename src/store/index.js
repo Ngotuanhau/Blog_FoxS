f547a047e77d069f661290e6e7567b2707d756d3
tuanhau <ngotuanhaudn@gmail.com>
2019-09-12 10:37:10 +0700

    feat(user): login
    Tạo snackbar bật thông báo lỗi
    
    see #3

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,12 @@
 import Vuex from "vuex";
 import Vue from "vue";
 
+import Snackbar from "./modules/Snackbar";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    modules: {}
+    modules: {
+        Snackbar
+    }
 });
