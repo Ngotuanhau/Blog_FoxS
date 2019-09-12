5adc317b75cb8a481c3c1c2886ada17217115318
tuanhau <ngotuanhaudn@gmail.com>
2019-09-12 15:19:55 +0700

    feat(slider): slide ảnh
    Tạo slider gồm 3 ảnh
    
    see #5

---
+++ src/components/Slider/Slider.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-carousel hide-delimiters cycle show-arrows-on-hover>
+    <v-carousel-item v-for="(item, index) in sliderImage" :key="index" :src="item.url"></v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sliderImage: []
+    };
+  },
+
+  mounted() {
+    this.getSlideImage();
+  },
+
+  methods: {
+    getSlideImage() {
+      axios.get("/media?folder=slider-image").then(response => {
+        this.sliderImage = response.data.media;
+        console.log(this.sliderImage);
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
