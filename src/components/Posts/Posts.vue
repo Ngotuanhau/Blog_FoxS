93905e9a5bd99ff19ef24ce010a9d532a042c217
tuanhau <ngotuanhaudn@gmail.com>
2019-09-13 10:35:28 +0700

    feat(posts): tạo trang posts
    Tạo trang posts hiển thị danh sách các bài viết
    Mỗi trang mặc định gồm 10 bài viết
    Những bài viết gần nhất sẽ hiển thị đầu trang
    
    see #7

---
+++ src/components/Posts/Posts.vue
@@ -0,0 +1,73 @@
+<template>
+  <v-container fluid grid-list-md>
+    <v-data-iterator :items="posts">
+      <template v-slot:default="props">
+        <v-layout wrap>
+          <v-flex
+            d-flex
+            xs12
+            sm6
+            md6
+            v-for="(item, index) in props.items"
+            :key="index"
+            class="pa-2"
+          >
+            <v-card width="100%" class="c-card">
+              <router-link :to="'/object/'+ item.slug">
+                <v-img :src="item.metadata.image.url" alt="hinhanh" height="300" class="c-img">
+                  <v-card-title class="c-title-post">
+                    <span class="overline c-text-post">{{item.title}}</span>
+                  </v-card-title>
+                  <v-card-title class="c-title-post">
+                    <span class="overline c-text-post">{{item.title}}</span>
+                  </v-card-title>
+                </v-img>
+              </router-link>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </template>
+    </v-data-iterator>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: []
+    };
+  },
+
+  mounted() {
+    this.getPosts();
+  },
+
+  methods: {
+    getPosts() {
+      axios.get("/objects").then(response => {
+        this.posts = response.data.objects.filter(
+          item => item.type_slug !== "authors"
+        );
+        console.log(this.posts);
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.c-text-post {
+  color: black;
+  text-align: left;
+}
+.c-title-post {
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  background-color: rgb(255, 255, 255, 0.6);
+}
+.c-img {
+  border-radius: 5px;
+}
+</style>
