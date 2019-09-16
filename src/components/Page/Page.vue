118257f1d885d93bee269539086872df263c99ee
tuanhau <ngotuanhaudn@gmail.com>
2019-09-16 16:06:19 +0700

    feat(related_post): Tạo danh sách các bài viết chung danh mục
    see #15

--- src/components/Page/Page.vue
+++ src/components/Page/Page.vue
@@ -1,20 +1,48 @@
 <template>
   <v-container fluid>
-    <v-layout v-for="(item, index) in page" :key="index" class="c-page">
+    <v-layout v-if="page" class="c-page">
       <v-flex class="c-page-title">
-        <span>{{item.title}}</span>
+        <span>{{page.title}}</span>
       </v-flex>
       <v-flex class="c-page-date">
-        <span>{{item.created_at | moment("calendar") }}</span>
+        <span>{{page.created_at | moment("calendar") }}</span>
       </v-flex>
       <v-flex class="c-page-content">
         <div>
-          <span v-html="item.content"></span>
+          <span v-html="page.content"></span>
         </div>
         <div>
-          <span>Theo: {{item.metadata.authors.title}}</span>
+          <span>Theo: {{page.metadata.authors.title}}</span>
         </div>
       </v-flex>
+    </v-layout>
+    <v-layout column mt-5>
+      <v-flex>
+        <span class="title c-related">Related posts</span>
+      </v-flex>
+      <v-container fluid grid-list-md>
+        <v-data-iterator
+          :items="pages_related"
+          :items-per-page.sync="itemsPerPage"
+          :footer-props="{ itemsPerPageOptions }"
+        >
+          <template v-slot:default="props">
+            <v-layout wrap>
+              <v-flex xs12 sm6 md4 v-for="(item, index) in props.items" :key="index">
+                <v-card width="100%" class="c-card">
+                  <router-link :to="'/page/'+ item.slug">
+                    <v-img :src="item.metadata.image.url" alt="hinhanh" height="300" class="c-img">
+                      <v-card-title class="c-title-post">
+                        <span class="overline c-text-post">{{item.title}}</span>
+                      </v-card-title>
+                    </v-img>
+                  </router-link>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </template>
+        </v-data-iterator>
+      </v-container>
     </v-layout>
   </v-container>
 </template>
@@ -23,8 +51,17 @@
 export default {
   data() {
     return {
-      page: []
+      page: null,
+      pages: [],
+      itemsPerPageOptions: [3, 6, 9],
+      itemsPerPage: 3
     };
+  },
+
+  computed: {
+    pages_related() {
+      return this.pages.filter(item => item.slug !== this.page.slug);
+    }
   },
 
   mounted() {
@@ -34,9 +71,23 @@ export default {
   methods: {
     getPage() {
       axios.get("/object/" + this.$route.params.slug).then(response => {
-        this.page = response.data;
+        this.page = response.data.object;
         console.log(this.page);
+        this.getPages();
       });
+    },
+
+    getPages() {
+      axios.get(`objects?type=${this.page.type_slug}`).then(response => {
+        console.log(response);
+        this.pages = response.data.objects;
+      });
+    }
+  },
+
+  watch: {
+    $route(val) {
+      this.getPage();
     }
   }
 };
@@ -61,5 +112,21 @@ export default {
 
 .c-page-content {
   text-align: left;
+}
+
+.c-text-post {
+  color: black;
+  text-align: left;
+}
+
+.c-title-post {
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  background-color: rgb(255, 255, 255, 0.6);
+}
+
+.c-img {
+  border-radius: 5px;
 }
 </style>
