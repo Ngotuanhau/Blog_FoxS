bb1bee5cde1d02f6da6f0f666a80fff480366d9f
tuanhau <ngotuanhaudn@gmail.com>
2019-10-01 14:27:26 +0700

    deploy

---
+++ precache-manifest.b242b9eb219e62f0c0cc570392881e60.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Blog_FoxS/robots.txt"
+  },
+  {
+    "revision": "cf1b6e669a9f3a062efd",
+    "url": "/Blog_FoxS/js/chunk-vendors.996e02c8.js"
+  },
+  {
+    "revision": "77801b0e9583b6fee9e7",
+    "url": "/Blog_FoxS/js/app.052b34bc.js"
+  },
+  {
+    "revision": "acecefbfe16cae6bf70824dcc5e6a8e9",
+    "url": "/Blog_FoxS/index.html"
+  },
+  {
+    "revision": "a32fa1f27abbfa96ff2f79e1ade723d5",
+    "url": "/Blog_FoxS/fonts/materialdesignicons-webfont.a32fa1f2.eot"
+  },
+  {
+    "revision": "a0d13d16cc2f3647680d9f1ff003f58b",
+    "url": "/Blog_FoxS/fonts/materialdesignicons-webfont.a0d13d16.woff"
+  },
+  {
+    "revision": "7ec5dab7e7ff250971d2ff50379778dc",
+    "url": "/Blog_FoxS/fonts/materialdesignicons-webfont.7ec5dab7.woff2"
+  },
+  {
+    "revision": "3ac50b5b36eb2f11b000dce1792d0bb0",
+    "url": "/Blog_FoxS/fonts/materialdesignicons-webfont.3ac50b5b.ttf"
+  },
+  {
+    "revision": "cf1b6e669a9f3a062efd",
+    "url": "/Blog_FoxS/css/chunk-vendors.8dc44a8b.css"
+  },
+  {
+    "revision": "77801b0e9583b6fee9e7",
+    "url": "/Blog_FoxS/css/app.91b58059.css"
+  }
+];
